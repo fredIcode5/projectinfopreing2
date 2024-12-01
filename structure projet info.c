8217375bfc0bebe39c5e62764d225a2139620ef4
fredIcode5 <168578739+fredIcode5@users.noreply.github.com>
2024-12-02 00:32:01 +0100

    Add files via upload

---
+++ structure projet info.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdio.h>
+
+
+
+typedef struct{
+hvb* p_main_hvb;// pointeur sur la la liste chainée de hvb 
+}centrale; 
+
+typedef struct{
+hva* p_main_hva// pointeur sur la la liste chainée de hva
+}hvb;
+
+typedef struct{
+
+}consomateur; 
+
+typedef struct{
+
+}poste lv;
