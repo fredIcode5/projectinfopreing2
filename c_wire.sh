0ec84077420f6f4367d32760f35d3c8d2680cee2
SweeTix <106099752+SweeTix@users.noreply.github.com>
2024-12-09 16:03:24 +0100

    shell gestion argument v1

--- c_wire.sh
+++ c_wire.sh
@@ -1,7 +1,131 @@
+#!/bin/bash
+
 #changer les - en 0 pour le C
-tr - 0
+#tr - 0
 
 #filtrer les lignes
-grep -o MOTIF data.csv
+#grep -o MOTIF data.csv 
+
+
+
+
+help(){
+	echo ◦ chemin du fichier de données :
+	echo 	• obligatoire
+	echo 	• indique l’endroit où se trouve le fichier d’entrée
+	echo ◦ type de station à traiter
+	echo 	• obligatoire
+	echo 	• valeurs possibles :
+	echo 		hvb "(high-voltage B)"
+	echo 		hva "(high-voltage A)"
+	echo 		lv "(low-voltage)"
+	echo ◦ type de consommateur à traiter
+	echo 	• obligatoire
+	echo 	• valeurs possibles :
+	echo 		comp "(entreprises)"
+	echo 		indiv "(particuliers)"
+	echo 		all "(tous)"
+	echo 	• ATTENTION : les options suivantes sont interdites car seules des entreprises sont connectées aux stations HV-B et HV-A :
+	echo 		hvb all
+	echo 		hvb indiv
+	echo 		hva all
+	echo 		hva indivecho 
+	echo ◦ Identifiant de centrale :
+	echo 	• optionnel
+	echo 	• filtre les résultats pour une centrale spécifique
+	echo 	• si cette option est absente, les traitements seront effectués sur toutes les centrales du fichier
+}
+
+for arg in $@
+do
+	if [[ $arg == "-h" ]] ; then
+		help()
+	fi
+done
+
+if [[ $# -ne 3 ] || [[ $# -ne 4 ] ; then
+do
+	echo "Erreur : mauvais nombre d'arguments"
+fi
+
+
+# Revision des if par un swicth case
+case "$2_$3" in
+	
+	"hvb_comp")
+		echo hvb_comp
+		;;
+		
+	"hva_comp")
+		echo hva_comp
+		;;
+	
+	"lv_comp")
+		echo lv_comp
+		;;
+	
+	"lv_indiv")
+		echo lv_indiv
+		;;
+	
+	"lv_all")
+		echo lv_all
+		;;	
+	
+	*)
+		echo "Erreur : arguments non cohérents"
+		help()
+		;;
+		
+esac
+
+
+
+# A revoir pour transformer les if en switch case
+centrale=0
+if [ -n $4 ] ; then
+	if [ $4 -gt 0 ] && [ $4 -lt 4 ] ; then
+		centrale=$4
+	fi
+fi
+
+if [[ "$2" == "-h" ]] ; then
+	help()
+
+elif [[ "$2" == 'hvb' ]] ; then
+	if [[ "$3" == "comp" ]] ; then
+		echo oui
+	else
+		echo Type de consommateur incorrect 
+		help()
+	fi
+	
+elif [[ "$2" == "hva" ]] ; then
+	if [[ "$3" == "comp" ]] ; then
+		echo oui
+	else
+		echo Type de consommateur incorrect
+		help()
+	fi
+	
+elif [[ "$2" == "lv" ]] ; then
+	if [[ "$3" == "comp" ]] ; then
+		echo oui
+	elif [[ "$3" == "indiv" ]] ; then
+		echo oui
+	elif [[ "$3" == "all" ]] ; then
+		echo oui
+	else
+		echo Type de consommateur incorrect
+		help()
+	fi
+	
+else
+	echo Type de station incorrect
+	help()
+fi
+
+
+
 
 
