bf1a2179f38d37e978871c4d254d23d478f57289
fredIcode5 <168578739+fredIcode5@users.noreply.github.com>
2024-12-13 00:12:17 +0100

    Add files via upload

---
+++ trihva.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Nom du fichier passé en argument
+file="$1"
+var="$2"
+touch "tempresult6.txt"
+
+if [[ "$var" == "hva" ]] ; then
+# tri hva 
+grep -E '^[^;]*;[^;]*;[^-;];[-]+' "$file"|cut -d';' -f7,8 
+>> tempresult6.txt 
+fi
+
+if [[ "$var" = "hvb" ]] ; then
+#hvb
+grep -E '^[^;]*;[^-;]*;[^;]*;[-]+' "$file"|cut -d';' -f7,8 
+ >> tempresult6.txt 
+fi
+
+if [[ "$var" = "lv" ]] ; then
+#lv
+grep -E '^[^;]*;[^;]*;[^;]*;[^-;];[-]+' "$file"|cut -d';' -f7,8
+ >> tempresult6.txt 
+fi
+
+
+#remplacer les tirt par des 0
+
+
