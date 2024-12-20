fc92d57db7755a8817675a353c9a785c5e12e0cd
Tom_ma95 <167717246+Drackfish@users.noreply.github.com>
2024-12-20 16:20:37 +0100

    Add files via upload

---
+++ passerelle.h
@@ -0,0 +1,36 @@
+#ifndef PASSERELLEJO_H_INCLUDED
+#define PASSERELLEJO_H_INCLUDED
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+typedef struct chainon{
+    int ID;
+    long long cap;
+    long long conso;
+}Chainon;
+
+typedef struct tree{
+    Chainon* c;
+    struct tree* pLeft;
+    struct tree* pRight;
+    int balance;
+}Tree;
+
+
+int max2(int v1, int v2)
+int min2(int v1, int v2)
+int max3(int v1, int v2, int v3)
+int min3(int v1, int v2, int v3)
+Chainon* createChainon(int id, long long CAP, long long CONSO)
+Tree* createAVL(Chainon* c)
+Tree* rotateLeft(Tree* pRoot)
+Tree* rotateRight(Tree* pRoot)
+Tree* doubleRotateLeft(Tree* pRoot)
+Tree* doubleRotateRight(Tree* pRoot)
+Tree* balanceAVL(Tree* pRoot)
+Tree* insertAVL(Tree* pRoot, Chainon* c, int* h)
+void infix(Tree* p)
+
+#endif // PASSERELLEJO_H_INCLUDED
