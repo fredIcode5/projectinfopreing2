27f86bb1d18a1530f7aa88feff8ab65c63031c3c
Tom_ma95 <167717246+Drackfish@users.noreply.github.com>
2024-12-21 18:48:33 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -4,11 +4,12 @@ int main(){
     Chainon* new;
     Tree* AVL;
     int id, h=0;
-    long long CAP;
-    long long CONSO;
+    long long CAP, CONSO;
     while(scanf("%d;%lld;%lld\n", &id, &CAP,&CONSO) == 3){
         new = createChainon(id, CAP, CONSO);
         AVL = insertAVL(AVL, new, &h);
     }
     infix(AVL);
+    printf("\n");
+    return 0;
 }
